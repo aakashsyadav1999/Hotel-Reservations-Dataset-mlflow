16e4edce9ba220f5dd00434720de8706852d8408
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-10-11 12:53:13 +0530

    Logger

--- src/Hotel_Reservation_Predicition/logger.py
+++ src/Hotel_Reservation_Predicition/logger.py
@@ -2,16 +2,12 @@ import logging
 import os
 from datetime import datetime
 
-#Log File define moth, day, year, hour, month, second
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
 
-#Define Directory where we want to apply logging
-log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-
-#Joining Log File
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
 
-#Basic Config
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
