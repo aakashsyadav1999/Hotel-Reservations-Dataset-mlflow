94bec8c2c8ba99b5eb8ce169c56c0ac11c77bd1b
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-10-12 17:31:12 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.Hotel_Reservation_Predicition.logger import logging
 from src.Hotel_Reservation_Predicition.exception import CustomException
 from src.Hotel_Reservation_Predicition.components.data_ingestion import DataIngestion,DataIngestionConfig
 from src.Hotel_Reservation_Predicition.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.Hotel_Reservation_Predicition.components.model_tranier import ModelTrainer,ModelTrainerConfig
 
 import sys
 
@@ -17,6 +18,11 @@ if __name__=="__main__":
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        #model trainer
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         raise CustomException(e,sys)
