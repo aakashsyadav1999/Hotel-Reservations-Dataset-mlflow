d131399db3cd9465cf17405e536d083301cd17a1
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-10-12 17:52:56 +0530

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ if __name__=="__main__":
         train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
         #model trainer
-
         model_trainer=ModelTrainer()
         print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
