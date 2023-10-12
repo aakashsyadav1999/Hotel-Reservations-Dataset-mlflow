fa99a175bb9b6ce453e400e952e0bd975c22bceb
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-10-12 17:51:12 +0530

    model_trainer.py

--- src/Hotel_Reservation_Predicition/components/model_tranier.py
+++ src/Hotel_Reservation_Predicition/components/model_tranier.py
@@ -54,7 +54,7 @@ class ModelTrainer:
                 "RandomForestClassifier":{
                    'n_estimators' : [100],
                 #    'n_jobs' : [-1],
-                   'max_features':[1,2,3],
+                   'max_features':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
                 #   'max_depth':[3, 4, 5, 6, 7, 9, 11],
                 #    'min_samples_split':[2,3]
                 }
